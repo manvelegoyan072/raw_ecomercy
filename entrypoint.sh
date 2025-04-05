977777dfb12b3624236deb9191bdccb8c4199070
Manvel <egoyanmanvel072@gmail.com>
2025-04-05 02:06:27 -0300

    перенес данные админа в .env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+
+export $(grep -v '^#' .env | xargs)
+
 # Применение миграций
 python manage.py makemigrations
 python manage.py migrate
 
 # Создание суперпользователя, если не существует
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-User.objects.filter(username='admin').exists() or \
-User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" \
-| python manage.py shell
+echo "
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+" | python manage.py shell
 
 # Запуск сервера
 python manage.py runserver 0.0.0.0:8000
